ac03a043a8081be4d55f8302a55a7926307b5bc8
Tad Tarleton <ttarlet2@my.westga.edu>
2015-02-10 04:23:19 -0500

    Random category, and question belonging to that category are now displayed on start page. Answers are displayed as buttons with the answer's is_correct set as its value. Answers are displayed in random order.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,6 @@
 class GameController < ApplicationController
   def start
     @random_category = Category.all.shuffle[0]
+    @random_question = @random_category.questions.all.shuffle[0]
   end
 end
