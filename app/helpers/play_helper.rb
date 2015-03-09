d1c6c6d3e40ded3bc92a72516e4be351adb670f2
Tad Tarleton <ttarlet2@my.westga.edu>
2015-03-08 22:39:36 -0400

    Cleaned up display questions

--- app/helpers/play_helper.rb
+++ app/helpers/play_helper.rb
@@ -1,2 +1,18 @@
 module PlayHelper
+  def true_answer
+    if @going_for_trophy == true
+      Player.first.trophies << @random_question.category.trophy
+      @going_for_trophy = false
+      Player.first.update_attribute(:meter, @meter + 1)
+    else
+      Player.first.update_attribute(:meter, @meter + 1)
+      if @meter > 2
+        @going_for_trophy = true
+        Player.first.update_attribute(:meter, 0)
+      end
+    end
+  end
+  def false_answer
+    @going_for_trophy = false
+  end
 end
