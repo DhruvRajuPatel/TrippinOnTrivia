8c2b301acc754dfb1e99cd5f31b78910f001e53b
Dhruv Patel <dhruvrajupatel93@gmail.com>
2015-03-29 08:00:37 -0400

    Added login changes again, also fixed issue of migrate by adding attributes to the migration file of the player class. Rake db:migrate now works.

--- db/migrate/20150308175710_create_players.rb
+++ db/migrate/20150308175710_create_players.rb
@@ -2,7 +2,8 @@ class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
       t.integer :meter
-
+      t.boolean  "isActivePlayer"
+      t.integer  "player_id"
       t.timestamps null: false
     end
   end
