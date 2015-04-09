c83ddfc40a4e57dc00fa5a75d48071049d50ecd8
ttarlet2 <ttarlet2@my.westga.edu>
2015-04-09 04:41:47 -0400

    Trying again

--- db/migrate/20150409054712_add_references_between_questions_answers_and_challenge.rb
+++
@@ -1,8 +0,0 @@
-class AddReferencesBetweenQuestionsAnswersAndChallenge < ActiveRecord::Migration
-  def change
-    add_column :challenges, :question_id, :integer
-    add_column :challenges, :answer_id, :integer
-    add_column :questions, :challenge_id, :integer
-    add_column :answers, :challenge_id, :integer
-  end
-end
