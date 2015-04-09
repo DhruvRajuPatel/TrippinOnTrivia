8e67c114992aa98e47d50f2986c7d97b2e82bf41
ttarlet2 <ttarlet2@my.westga.edu>
2015-04-09 04:36:50 -0400

    Trying again

--- db/migrate/20150409045051_the_big_one.rb
+++ db/migrate/20150409045051_the_big_one.rb
@@ -2,8 +2,6 @@ class TheBigOne < ActiveRecord::Migration
   def change
     remove_column :users, :next_level_threshold
     add_column :challenges, :question_counter, :integer
-    add_column :challenges, :trophy_id, :integer
-    add_column :trophies, :challenge_id, :integer
     add_column :players, :challenger_player_id, :integer
     add_column :players, :challenged_player_id, :integer
     add_column :players, :winner_player_id, :integer
