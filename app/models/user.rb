eaac31db483d5c25d58e70ee01833e367c4064a5
Dhruv Patel <dhruvrajupatel93@gmail.com>
2015-04-22 16:04:08 -0400

    Trying to fix Heroku.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-
+  attr_accessor :avatar
   has_attached_file :avatar, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
